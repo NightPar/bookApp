4ff6d02615017aa315f643c432c16f04910f1d7c
NightPar <1027869635@qq.com>
2016-12-05 10:28:30 +0800

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,3 @@
+(function(){
+    angular.module("bookApp",["ngRoute","ngAnimate","ngSanitize","bookDataModule","appRouteModule","getDataModule","bookListCtrlModule","hostUrlModule","bookDetailCtrlModule","bookCartCtrlModule","localStorageModule","postDataModule","loginCtrlModule","bookOrderCtrlModule"]);
+})();
